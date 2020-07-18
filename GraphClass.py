b73dbd5284f41f2627c3c756fd8edbb59fa52f68
unknown <mmghho77@gmail.com>
2020-07-18 20:49:58 +0430

    some changes

--- GraphClass.py
+++ GraphClass.py
@@ -4,7 +4,7 @@ class GraphColoring:
         self.adjMatrix = adjMatrix
         self.colorNumber = colorNumber
         self.nodeNumber = len(adjMatrix)
-        self.colorMatrix = [-1]*nodeNumber
+        self.colorMatrix = [-1]*self.nodeNumber
         self.colors = [i for  i in range(colorNumber)]
 
     def show(self):
@@ -15,38 +15,42 @@ class GraphColoring:
     def coloring(self):
         root = node(None,0,0)
         self.colorMatrix[0] = 0
-        current = root
-        for i in range(self.nodeNumber)
-            colors = self.colors
-            self.forwardChecking(colors, i)
-            if len(colors) > 0:
-                check = False
-                for j in range(self.nodeNumber)
-                    if self.adjMatrix[i][j] == '1' and self.colorMatrix[j] == -1:
-                        check = True
-                        for x in range(len(colors))
-                            newNode = node(current, j, colors[x])
-                            current.sons.append(newNode)
-                        
-                        current = current.sons[0]
-                        self.colorMatrix[j] = current.color
-                        break
+        currentNumber = 0
+        self.coloringSubFunction(currentNumber, root)
+        self.showResult(1)     
 
-                if not check:
-                    self.colorMatrix[i] = 0
-                if check:   
-                    i = current.number
+    def coloringSubFunction(self, currentNumber, current):
+        colors = self.colors
+        self.forwardChecking(colors, currentNumber)
+        if len(colors) > 0:
+            check = False
+            for j in range(self.nodeNumber):
+                if self.adjMatrix[currentNumber][j] == '1' and self.colorMatrix[j] == -1 and not check:
+                    check = True
+                    print(">> Node[",current.number+1,"] : color:",colors,"")
+                    print(">> Node[",current.number+1,"] : color[",current.color,"]") 
+                    for x in range(len(colors)):
+                        newNode = node(current, j, colors[x])
+                        current.sons.append(newNode)
                     
-            else: # there is no color
-                if current != root:
-                    self.colorMatrix[current.number] = -1
-                    current = current.prvNode
-                    
+                    current = current.sons[0]
+                    self.colorMatrix[j] = current.color
+                    currentNumber = current.number
+                    self.forwardChecking(colors, current.number)
+                    break
+            
+            if not check:
+                self.colorMatrix[currentNumber] = 0
+            self.coloringSubFunction(currentNumber, current)
+        else: # there is no color
+            return False        
 
     def forwardChecking(self, colors, nodeNumber):
-         for k in range(self.nodeNumber)
-            if self.adjMatrix[i][k] == '1':
-                colors.remove(self.colorMatrix[j]) #remove connection colors
+        for k in range(self.nodeNumber):
+            if (self.adjMatrix[nodeNumber][k] == '1'):
+                if self.colorMatrix[k] in colors: colors.remove(self.colorMatrix[k]) #remove connection colors
+
+        print("> Node[",nodeNumber+1,"]: color:",colors)
 
     def arcConsistancyChecking(self):
         print("arcConsistancyChecking")
@@ -55,7 +59,7 @@ class GraphColoring:
     def showResult(self, status):
         if (status == 1):
             print(">> Result:")
-            for x in range(self.colorNumber):
+            for x in range(len(self.colorMatrix)):
                 print(">>>> Node[",x+1,"] : Color[",self.colorMatrix[x],"]")
         else:
             print("There is no way to coloring the graph with these number of colors.")
--- sam.py
+++ sam.py
@@ -1,5 +1,5 @@
-# from GraphClass import GraphColoring
-# from node import node     
+from GraphClass import GraphColoring
+    
 #input paramiters--------------------------
 #the Graph Adjacent Matrix has default value:
 inputMatrix =   [
@@ -12,34 +12,19 @@ inputMatrix =   [
 colorNumber = 5
 #------------------------------------------
 # get number of color
-# colorNumber = int(input("Enter Color number : "))
-# # read matrix as text in 
-# with open("inputMatrix.txt") as textFile:
-#     lines = [line.split() for line in textFile]
-# print(lines)
-# matrixLen = len(lines)
-# # check if number of color more than the number of node it isn't need the calculation
-# if (colorNumber >= matrixLen):
-#     for x in range(matrixLen):
-#         print("Node[", x+1, "]: Color[", x+1, "]")
-# elif (colorNumber <= 0):
-#     print("ERROR: The Color numebrs must be more than 0")
-# else: # The main calculation
-#     obj1 = GraphColoring(lines, colorNumber)
-#     obj1.show()
-
-# root = node(None,1,5)
-# node1 = node(root,2,6)
-# node2 = node(root,2,1)
-# root.sons.append(node1)
-# root.sons.append(node2)
-# node2.sons.append(node1)
-# print(root.sons[1].sons[0].color)
-
-
-
-colors = [i for  i in range(1)]
-colors.remove(0)
-if len(colors) >= 1:
-    print("yes")
-print(colors)
+colorNumber = int(input("Enter Color number : "))
+# read matrix as text in 
+with open("inputMatrix.txt") as textFile:
+    lines = [line.split() for line in textFile]
+print(lines)
+matrixLen = len(lines)
+# check if number of color more than the number of node it isn't need the calculation
+if (colorNumber >= matrixLen):
+    for x in range(matrixLen):
+        print("Node[", x+1, "]: Color[", x+1, "]")
+elif (colorNumber <= 0):
+    print("ERROR: The Color numebrs must be more than 0")
+else: # The main calculation
+    obj1 = GraphColoring(lines, colorNumber)
+    obj1.show()
+    obj1.coloring()
